c4550eb2924b06f37e25af29a642675bcf407c35
Pedro Jose <macintoshmac@macbook-pro-de-macintosh.local>
2016-04-03 22:39:13 -0500

    Programa un playground que evalúe un rango de números en base a 4 reglas de impresión. Generar un rango de 0 a 100, incluye el número 100 en el rango. Tienes que iterar el rango completo, utilizando la sentencia for para obtener cada número del rango y aplicar las siguientes reglas de impresión. Al evaluar cada número debes aplicar las siguientes reglas:           - Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”           - Si el número es par, imprime: # el número + “par!!!”           - Si el número es impar, imprime: # el número + “impar!!!”           - Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!” Debes de usar la interpolación de variables para realizar la impresión de cada número.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let rango = 0...100
+for i in rango{
+    let divisible5 = i % 5
+    if divisible5 == 0
+    {
+        print("# \(i) Bingo!!!")
+    }
+    
+    let par = i % 2
+    if par == 0
+    {
+        print("# \(i) par!!!")
+    }
+    else
+    {
+        print("# \(i) impar!!!")
+    }
+    
+    if i >= 30 && i<=40
+    {
+        print("# \(i) Viva Swift!!!")
+    }
+}
+
+
+
+
+
+
+
+
